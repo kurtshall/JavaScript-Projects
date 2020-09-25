010d2944bdb1c75f815a2f00346f3c8e5d1e2d67
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-09-25 10:28:24 -0700

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello,world");// and alert to say hello world
+document.write("How is it going?"
++ "it feels like today is good"); // another alert with two statments
+var A = "This is a alert \"string\"" + "and now more of it"; B = "This is for B"; C = "This is for C";
+document.write(A); //printing out value A
+window.alert(C) // making an alert for value C
+var reds = "This is the reds baseball team";
+var reds = reds.fontcolor("red");
+document.write(reds);// created red text to the reds value
+document.write(4+3); //this is an expresion
