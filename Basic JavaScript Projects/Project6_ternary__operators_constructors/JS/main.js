3bb67e795b1786dde980bd87365b8234fceefc1a
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-09-29 20:31:47 -0700

    Update main.js

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,26 +1,26 @@
-function count_Function () {
-    document.getElementById("Nested_Function").innerHTML = Count();
-    function Count () {
-        var Start = 4;
-        function addition() {Start += 1;}
-        addition();
-        return Start;
+function count_Function () { //this is the nested loop
+    document.getElementById("Nested_Function").innerHTML = Count();//this is to be read out to the user, once the inner loop is ran.
+    function Count () { //inner loop function
+        var Start = 4; // setting initial value
+        function addition() {Start += 1;} //making the addition function
+        addition(); //running the addition function
+        return Start;// returning Start to be sent back as the value of the start of the inner loop.
     }
 }
 
 
 
 
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
+function Vehicle(Make, Model, Year, Color) { //setting the vehical with each of its properties
+    this.Vehicle_Make = Make;//setting propertie
+    this.Vehicle_Model = Model;//setting propertie
+    this.Vehicle_Year = Year;//setting propertie
+    this.Vehicle_Color = Color;//setting propertie
 }
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red"); //assigning values to each of the properties in order of the Vehical Function input
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-function myFunction() {
+function myFunction() { // this portion read out each of the selected properties given the name of the person.
     document.getElementById("Keywords_and_Constructors").innerHTML =
     "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
     " manufactured in " + Erik.Vehicle_Year;
@@ -30,12 +30,12 @@ function myFunction() {
 
 
 
-
-function Ride_Function() {
+//This is the the vote function
+function Vote_Function() {
     var Age, Can_vote;
-    Age = document.getElementById("Age").value;
-    Can_vote = (Age < 18) ? "You are not Old enough to vote.":"You can Vote!";
-    document.getElementById("Ride").innerHTML = Can_vote;
+    Age = document.getElementById("Age").value; //this takes the value that was inputed and sets it to age
+    Can_vote = (Age < 18) ? "You are not Old enough to vote.":"You can Vote!"; // this compares to see if you are old enough to vote or not
+    document.getElementById("Vote").innerHTML = Can_vote; // this displays back to the user if they can vote or not
 
 }
 
